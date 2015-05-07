7e227c4fade5482fae17166dd3f50d196d562545
Adnan Begovic <adnan@cyngn.com>
2015-05-07 15:21:09 -0700

    curve25519: Create curve25519-java module in cmake.
    Change-Id: I3b0feda715f5ffded0a9751606399c0d763808cd

---
+++ Android.mk
@@ -0,0 +1,29 @@
+# Copyright (C) 2015 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+LOCAL_PATH := $(call my-dir)
+include $(CLEAR_VARS)
+
+# The curve25519 java module
+LOCAL_MODULE := curve25519-java
+
+src_files := java/src/main/java
+common := common/src/main/java
+
+LOCAL_SRC_FILES := \
+    $(call all-java-files-under, $(common)) \
+    $(call all-java-files-under, $(src_files))
+
+include $(BUILD_STATIC_JAVA_LIBRARY)
+
